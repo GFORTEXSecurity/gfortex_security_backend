6c9ea4d946d4beeac7b37810c8dd3f93ae5a554d
Janine Günther <g.fortex.sec@gmail.com>
2025-02-13 23:44:18 +0100

    Datenbankverbindung hinzugefügt

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+import os  # Hier oben einfügen!
+from sqlalchemy import create_engine
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Die API läuft!"}
+
+# 📌 Datenbank-Verbindung
+DATABASE_URL = os.getenv("DATABASE_URL")  # Holt die URL aus den Umgebungsvariablen
+engine = create_engine(DATABASE_URL)
 
